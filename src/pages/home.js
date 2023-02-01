243f322fb47f8a97928dee03aed867c555d1b128
cherelemma <73167960+cherelemma@users.noreply.github.com>
2023-02-01 18:17:12 +0300

    debug and fix linters error

--- src/pages/home.js
+++ src/pages/home.js
@@ -2,7 +2,7 @@ const Home = () => (
   <div className="home-content">
     <h1>Welcome to our page!</h1>
     <p>
-      {" "}
+      {' '}
       Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
       tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
       veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
@@ -12,7 +12,7 @@ const Home = () => (
       est laborum
     </p>
     <p>
-      {" "}
+      {' '}
       Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
       tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
       veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
